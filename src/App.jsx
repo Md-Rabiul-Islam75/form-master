761ece9fcab36e22a79e5998f38fd424bedf7ff7
Md. Rabiul Islam <rabiul77777888@gmail.com>
2024-11-03 18:17:47 +0600

    custom hook and create your first custom hook

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+// 46-1 Handle form with onSubmit and access field data
+import './App.css'
+import HookForm from './Components/HookForm/HookForm'
+//import RefForm from './Components/RefForm/RefForm'
+//import StatefulForm from './Components/StatefulForm/StatefulForm'
+//import SimpleForm from './Components/SimpleForm/SimpleForm'
+
+function App() {
+
+
+  return (
+    <>
+      
+      <h1>Vite + React</h1>
+      {/* <SimpleForm></SimpleForm> */}
+      
+      {/* <StatefulForm></StatefulForm> */}
+
+      {/* <RefForm></RefForm> */}
+
+      <HookForm></HookForm>
+    </>
+  )
+}
+
+export default App
